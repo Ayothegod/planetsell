72852d6edaf17c7e33da425647bbf8f08c43783c
Ayo <heyayomideadebisi@gmail.com>
2024-03-12 02:44:58 +0100

    build: initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <div>
+      <p>Hello coders</p>
+      <p>Lets build something</p>
+
+      <div>
+      </div>
+    </div>
+  );
+}
---
+++ src/lib/actions.ts
@@ -0,0 +1,27 @@
+"use server";
+
+// async mutations here
+// import { auth, signIn, signOut } from "@/auth";
+
+// export async function signInGoogleAction() {
+//   return await signIn("google", {
+//     redirectTo: "/dashboard",
+//     redirect: true,
+//   });
+// }
+
+// export async function signOutAction() {
+//   return await signOut({
+//     redirectTo: "/",
+//     redirect: true,
+//   });
+// }
+
+// export async function giveFreeLunch(formData: FormData){
+//   try {
+    
+//   } catch (error) {
+//     console.log(error);
+//     throw new Error("Cant fetch single user");
+//   }
+// }
