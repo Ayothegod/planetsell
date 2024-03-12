12a7b2ff4d402507d70242c84c41e7e4352fbfc2
Ayo <heyayomideadebisi@gmail.com>
2024-03-12 03:11:05 +0100

    chore: cleanup code

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -1,17 +1,1 @@
-// async fetch calls here
-
 // import { unstable_noStore as noStore } from "next/cache";
-
-// export async function getSingleUserById(id: any) {
-//   noStore();
-//   try {
-//     const db = await dbInstance();
-//     const user = await db
-//       .collection("users")
-//       .findOne({ _id: new ObjectId(id) });
-//     return user;
-//   } catch (error) {
-//     console.log(error);
-//     throw new Error("Cant fetch single user");
-//   }
-// }
