07ff519cac321a7066d404ddeb0c8b34474bcdce
Ayo <heyayomideadebisi@gmail.com>
2024-03-25 12:43:03 +0100

    task: move drizzle config into db folder

--- src/migrate.ts
+++
@@ -1,9 +0,0 @@
-// import { migrate } from "drizzle-orm/node-postgres/migrator";
-
-// import {db, client} from './db/migrate';
-// // This will run migrations on the database, skipping the ones already applied
-// await migrate(db, { migrationsFolder: './db/migrations' });
-// // Don't forget to close the connection, otherwise the script will hang
-// await client.end();
-
-// console.log("error");
